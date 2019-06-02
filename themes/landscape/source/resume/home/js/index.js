173fbe826ebc4d07ba1fd274ba5456ed3aeb211e
zhaoyang <18355403288@163.com>
2019-06-02 12:58:35 +0800

    update

--- themes/landscape/source/resume/home/js/index.js
+++ themes/landscape/source/resume/home/js/index.js
@@ -22,14 +22,14 @@ $(function() {
     $(" h3.fade").addClass("fadesin2");
     $(" span.fade").addClass("fadesin3");
 
-    var _top;
-    var top1 = $("#section2").offset().top-30;
-    var top2 = $("#section3").offset().top-30;
-    var top3 = $("#section4").offset().top-30;
-    var top4 = $("#section5").offset().top-30;
-    var top5 = $("#section6").offset().top-30;
-    var top6 = $("#section7").offset().top-30;
-    var tops = [top1,top2,top3,top4,top5,top6];
+    // var _top;
+    // var top1 = $("#section2").offset().top-30;
+    // var top2 = $("#section3").offset().top-30;
+    // var top3 = $("#section4").offset().top-30;
+    // var top4 = $("#section5").offset().top-30;
+    // var top5 = $("#section6").offset().top-30;
+    // var top6 = $("#section7").offset().top-30;
+    // var tops = [top1,top2,top3,top4,top5,top6];
     $(window).resize();
 
     //回到顶部
